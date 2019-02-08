756d2715e694574b160a9a7aa317cbe9b9ad0b07
Swarajsing Haulkory <shaulkory@astek.mu>
2019-02-08 15:10:12 +0400

    Attach images ok

--- app/src/main/java/mu/astek/database/khadundentalcare/Activities/AddTreatmentActivity.java
+++ app/src/main/java/mu/astek/database/khadundentalcare/Activities/AddTreatmentActivity.java
@@ -1,12 +1,26 @@
 package mu.astek.database.khadundentalcare.Activities;
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 import mu.astek.database.khadundentalcare.DTO.AppointmentDTO;
 import mu.astek.database.khadundentalcare.DTO.TreatmentDTO;
@@ -20,7 +34,9 @@ public class AddTreatmentActivity extends AppCompatActivity {
     Button btnSave;
     Boolean isEdit = false;
     DatabaseService service;
-
+    TextView txtFiles;
+    int SELECT_PICTURES = 5;
+    public static final String TAG = "PhotoUtils";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +45,7 @@ public class AddTreatmentActivity extends AppCompatActivity {
         btnSave = findViewById(R.id.btnSave);
         txtDetails = findViewById(R.id.txtDetails);
         txtFees = findViewById(R.id.txtFees);
+        txtFiles = findViewById(R.id.txtFiles);
 
         appointment = (AppointmentDTO) getIntent().getSerializableExtra("appointment");
         isEdit = getIntent().getBooleanExtra("edit", false);
@@ -59,6 +76,16 @@ public class AddTreatmentActivity extends AppCompatActivity {
             }
         });
 
+        txtFiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*"); //allows any image file type. Change * to specific extension to limit it
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURES);
+            }
+        });
+
         if (isEdit && appointment.getTreatment() != null) {
             txtDetails.setText(appointment.getTreatment().getDetails());
             txtFees.setText(appointment.getTreatment().getFees() + "");
@@ -70,4 +97,101 @@ public class AddTreatmentActivity extends AppCompatActivity {
             txtFees.setSelection(pos2);
         }
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_PICTURES) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data.getClipData() != null) {
+                    int count = data.getClipData().getItemCount(); //evaluate the count before the for loop --- otherwise, the count is evaluated every loop.
+                    Uri imageUri = null;
+                    for (int i = 0; i < count; i++){
+                        imageUri = data.getClipData().getItemAt(i).getUri();
+                        savePhotoProfile("File"+i+".jpg",getBytes(imageUri));
+                    }
+
+                    //do something with the image (save it to some directory or whatever you need to do with it here)
+
+                }
+            } else if (data.getData() != null) {
+                String imagePath = data.getData().getPath();
+                //do something with the image (save it to some directory or whatever you need to do with it here)
+            }
+        }
+    }
+
+    public final static void savePhotoProfile(String photoName, byte[] photo) {
+        Log.i(TAG, "Saving photo:" + photoName);
+        File dir = getOutputMediaDirectoryProfilePic();
+        if (dir != null && photo != null) {
+            File photoFile = new File(dir, photoName);
+            FileOutputStream fos = null;
+            try {
+
+                fos = new FileOutputStream(photoFile);
+                fos.write(photo);
+
+            } catch (Exception e) {
+                Log.e(TAG, "Error writing profile photo with photoName: " + photoName);
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        }
+    }
+    public final static File getOutputMediaDirectoryProfilePic() {
+        // External sdcard location
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), ".Dentist");
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.e(TAG, "Error creating media storage directory");
+                return null;
+            }
+        }
+        return mediaStorageDir;
+    }
+
+
+    public byte[] getBytes(Uri data)  {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream(new File(getImagePath(data)));
+            byte[] buf = new byte[1024];
+            int n;
+            while (-1 != (n = fis.read(buf)))
+                baos.write(buf, 0, n);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        byte[] bbytes = baos.toByteArray();
+        return bbytes;
+    }
+
+    public  String getImagePath(Uri uri){
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":")+1);
+        cursor.close();
+
+        cursor = getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
+    }
+
 }
+
--- app/src/main/java/mu/astek/database/khadundentalcare/MainActivity.java
+++ app/src/main/java/mu/astek/database/khadundentalcare/MainActivity.java
@@ -2,17 +2,23 @@ package mu.astek.database.khadundentalcare;
 
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import mu.astek.database.khadundentalcare.Activities.AddAppointmentActivity;
 import mu.astek.database.khadundentalcare.Activities.AddPatientActivity;
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity
     DrawerLayout drawer;
 
     DatabaseService databaseService;
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +42,22 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if (Build.VERSION.SDK_INT >= 23)
+        {
+            if (checkPermission())
+            {
+                // Code for above or equal 23 API Oriented Device
+                // Your Permission granted already .Do next code
+            } else {
+                requestPermission(); // Code for permission
+            }
+        }
+        else
+        {
 
-
+            // Code for Below 23 API Oriented Device
+            // Do next code
+        }
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -89,5 +110,36 @@ public class MainActivity extends AppCompatActivity
         transaction.addToBackStack(null);
         transaction.commit();
         drawer.closeDrawer(GravityCompat.START);
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(MainActivity.this, "Write External Storage permission allows us to do store images. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("value", "Permission Granted, Now you can use local drive .");
+                } else {
+                    Log.e("value", "Permission Denied, You cannot use local drive .");
+                }
+                break;
+        }
     }
 }
