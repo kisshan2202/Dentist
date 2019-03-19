259d1f5e04347bbc2a5e1c984cdd57921a7c15f7
Swarajsing Haulkory <shaulkory@astek.mu>
2019-03-19 16:02:08 +0400

    Sync and download ok

--- app/src/main/java/mu/astek/database/khadundentalcare/DownloadFragment.java
+++ app/src/main/java/mu/astek/database/khadundentalcare/DownloadFragment.java
@@ -1,16 +1,27 @@
 package mu.astek.database.khadundentalcare;
 
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -41,6 +52,9 @@ public class DownloadFragment extends Fragment {
     DatabaseReference reference2;
     int totalPatient = 0;
     int appointments = 0;
+    ProgressBar progressBar;
+    Boolean hascompletedPatient = false;
+    Boolean hascompletedAppointment = false;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_dwl, container, false);
@@ -51,63 +65,14 @@ public class DownloadFragment extends Fragment {
         this.context = getActivity();
         this.databaseService = new DatabaseService(context);
         buttonSync = view.findViewById(R.id.buttonSync);
+        progressBar = view.findViewById(R.id.progressBar);
         buttonSync.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                if(!TypeHelper.isConnected(context)){
                     Toast.makeText(context,"No network connection available",Toast.LENGTH_SHORT).show();
                 }else {
-                    databaseService.deleteAllData();
-                    reference  = FirebaseDatabase.getInstance().getReference("Patient");
-                    valueEventListener = reference.addValueEventListener(new ValueEventListener() {
-                       @Override
-                       public void onDataChange(DataSnapshot snapshot) {
-                           int x = (int)snapshot.getChildrenCount();
-                           for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                               PatientDTO patientDTO = dataSnapshot.getValue(PatientDTO.class);
-                               databaseService.createPatient(patientDTO);
-                               totalPatient++;
-                           }
-
-
-                           if(totalPatient == x){
-                               Toast.makeText(context,"Patients Saved",Toast.LENGTH_SHORT).show();
-                           }
-                       }
-
-                       @Override
-                       public void onCancelled(DatabaseError databaseError) {
-
-                       }
-                   });
-
-                   reference2  = FirebaseDatabase.getInstance().getReference("Appointments");
-                   valueEventListener2 = reference2.addValueEventListener(new ValueEventListener() {
-                       @Override
-                       public void onDataChange(DataSnapshot snapshot) {
-                           int x = (int)snapshot.getChildrenCount();
-                           for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                               AppointmentDTO appointmentDTO = dataSnapshot.getValue(AppointmentDTO.class);
-                               databaseService.createAppointment(appointmentDTO);
-
-                               if(appointmentDTO.getTreatment()!= null){
-                                   databaseService.createTreatment(appointmentDTO.getTreatment());
-                               }
-                               appointments++;
-                           }
-
-
-                           if(appointments == x){
-                               Toast.makeText(context,"Appointments Saved",Toast.LENGTH_SHORT).show();
-                           }
-                       }
-
-                       @Override
-                       public void onCancelled(DatabaseError databaseError) {
-
-                       }
-                   });
-
+                   openPopup();
                 }
 
 
@@ -116,6 +81,63 @@ public class DownloadFragment extends Fragment {
 
 
         return view;
+    }
+
+    private void openPopup() {
+        final Dialog dialog = new Dialog(getActivity(), R.style.WalkthroughTheme);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.popup);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setCancelable(false);
+
+        dialog.setOnKeyListener(new Dialog.OnKeyListener() {
+
+            @Override
+            public boolean onKey(DialogInterface arg0, int keyCode,
+                                 KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    dialog.dismiss();
+                }
+                return true;
+            }
+        });
+
+        final EditText txtPassword;
+        txtPassword = dialog.findViewById(R.id.txtPassword);
+
+        Button btnCancel = dialog.findViewById(R.id.btnCancel);
+        Button btnContinue = dialog.findViewById(R.id.btnContinue);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(txtPassword.getText()) || !TextUtils.equals(txtPassword.getText(),"lallmatie")){
+                    txtPassword.setError("Invalid Password");
+                }else{
+                    downloadData();
+                    dialog.dismiss();
+                }
+            }
+        });
+
+
+        if (!getActivity().isFinishing()) {
+            dialog.show();
+        }
+    }
+
+    private void handleProgressbard() {
+        if(hascompletedAppointment && hascompletedPatient){
+            progressBar.setVisibility(View.INVISIBLE);
+            Toast.makeText(context,"Data has been downloaded from server",Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -128,5 +150,61 @@ public class DownloadFragment extends Fragment {
         }
         super.onStop();
 
+    }
+
+    public void downloadData(){
+        progressBar.setVisibility(View.VISIBLE);
+        databaseService.deleteAllData();
+        reference  = FirebaseDatabase.getInstance().getReference("Patient");
+        valueEventListener = reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                int x = (int)snapshot.getChildrenCount();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    PatientDTO patientDTO = dataSnapshot.getValue(PatientDTO.class);
+                    databaseService.createPatient(patientDTO);
+                    totalPatient++;
+                }
+
+
+                if(totalPatient == x){
+                    hascompletedPatient = true;
+                    handleProgressbard();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        reference2  = FirebaseDatabase.getInstance().getReference("Appointments");
+        valueEventListener2 = reference2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                int x = (int)snapshot.getChildrenCount();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    AppointmentDTO appointmentDTO = dataSnapshot.getValue(AppointmentDTO.class);
+                    databaseService.createAppointment(appointmentDTO);
+
+                    if(appointmentDTO.getTreatment()!= null){
+                        databaseService.createTreatment(appointmentDTO.getTreatment());
+                    }
+                    appointments++;
+                }
+
+
+                if(appointments == x){
+                    hascompletedAppointment = true;
+                    handleProgressbard();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
